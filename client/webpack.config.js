83d31455ace6dafb3aea8b0c65809a292f6d3b03
m1taylor <m1taylor@ryerson.ca>
2023-03-22 00:33:11 -0400

    webpack config start

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,7 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+//Adds and configures workbox plugins for a service worker and manifest file.
 
 module.exports = () => {
   return {
@@ -18,7 +17,40 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      /*generates HTML file that will include all webpack bundles using script tags a
+      will also set the title of the page to 'Contact Cards'*/
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Contact Cards'
+      }),
+
+      //creates InjectManifest object.
+    //used to configure how service worker is generated/injected into app
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+//makes json tgat orivudes info about app
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Text Editor',
+        short_name: 'TE',
+        description: 'Another Text Editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            /* icon that will be used for app, has diff sizes. 
+            The destination for this icon will be in the assets/icons folder*/
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
