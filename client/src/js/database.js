81e88e8d5d827ccbb930e859a933792d622ee19a
m1taylor <m1taylor@ryerson.ca>
2023-03-22 00:18:14 -0400

    first half of database done

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,8 +12,24 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Logic to method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+
+  console.log('Post to the database');
+//opens the database with the name "jate" and version 1
+  const textDB = await openDB("jate", 1);
+
+  const tx = textDB.transaction("jate", "readwrite");
+//creates a transaction with the objectStore "jate"
+  const store = tx.objectStore("jate");
+//saves an object with an id of 1 and a value of content
+  const request = store.put({ id: 1, value: content });
+//logs out the result of the request
+  const result = await request;
+  console.log('data saved to the db 🚀 ', result);
+  console.error('Not implemented');
+
+}
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
